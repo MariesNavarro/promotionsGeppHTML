1dd4c251a9009aca1647f6e2ecbacde68857ae92
Mn-Macbook18 <mn@Mariess-MacBook-Pro.local>
2019-03-29 14:05:29 -0600

    configurador (hasta paso 4)
    Cambie:
    1. ui/css/master.css
    2. ui/js/main.js
    Agregue:
    1. config.html
    2.ui/img/covers <--CARPETA
    3.ui/img/ic/check.svg
    4.ui/img/ic/menu-arrow.svg
    5.ui/img/ic/menu-close copy.svg
    6.ui/img/ic/menu-close.svg
    6.ui/img/ic/upload.svg

--- ui/js/main.js
+++ ui/js/main.js
@@ -62,18 +62,202 @@ function getPassForgot(){
   console.log("Recuperar correo");
 }
 
-function openLinks(c,t){
-  var p = t.parentElement.parentElement,
-      ul = p.lastElementChild;
-  if(c === "open"){
-    t.setAttribute("onclick", "openLinks('close', this)");
-    p.style.height = "100px";
-    ul.style.height = "50px";
-    ul.style.display = "block";
-  } else if (c === "close") {
-    t.setAttribute("onclick", "openLinks('open', this)");
-    p.style.height = "auto";
-    ul.style.height = "0";
-    ul.style.display = "none";
+function menuConfig(e){
+  var w = _("#extMenu");
+  if(e === "open"){
+    w.style.left = "0";
+  } else if (e === "close") {
+    w.style.left = "-100vw";
+  }
+}
+
+function hideMenuConfig(e, t){
+  var m, mSub, w, foot, logo, dragon, index, ul;
+  var elems = [m = _("#menuConfigNav"),
+              mSub = _("#menuConfig"),
+              index = _("#indexConfig"),
+              ul = _("#menuConfig>ul"),
+              w = _("#configWrap"),
+              foot = _("#configFoot"),
+              logo = _("#menuConfigNav > div> .logo"),
+              dragon = _("#dragonConfig")];
+  if(e === "hide"){
+      t.setAttribute("onclick", "hideMenuConfig('show', this)");
+      m.style.width  = "50px";
+      w.style.width = "calc(100% - 50px)";
+      mSub.style.margin = "5px";
+      index.style.top = "120px";
+      ul.style.height = "100px";
+      dragon.style.display = "block";
+      logo.style.display = "none";
+      foot.style.display = "none";
+  } else if(e === "show") {
+    t.setAttribute("onclick", "hideMenuConfig('hide', this)");
+    for (var i = 0; i < elems.length; i++) {
+      elems[i].setAttribute("style", " ");
+    }
+  }
+}
+
+function loadFileName(t){
+  var file = t.nextElementSibling,
+      fileNameW = t.children[1];
+  file.addEventListener("change", function(){
+    fileNameW.innerHTML = file.files[0].name;
+  });
+}
+
+function sliderConfigFun(e){
+  var w = _("#sliderConfig"),
+      bullet = __(".bulletStep>circle");
+      clearBullets();
+      completedSteps(e);
+      if(e < 5){
+        bullet[e].setAttribute("class", "stepInProgress trans5");
+      }
+  switch (e) {
+    case 0:
+     w.style.marginLeft = "0";
+    break;
+    case 1:
+     w.style.marginLeft = "-100%";
+    break;
+    case 2:
+     w.style.marginLeft = "-200%";
+    break;
+    case 3:
+     w.style.marginLeft = "-300%";
+    break;
+    case 4:
+     w.style.marginLeft = "-400%";
+    break;
+  }
+  function clearBullets(){
+    for (var i = 0; i < bullet.length; i++) {
+      bullet[i].setAttribute("class", "stepUncomplete trans5");
+    }
+  }
+  function completedSteps(e){
+    for (var i = e; i > 0; i--) {
+      bullet[i-1].setAttribute("class", "stepComplete trans5");
+    }
+  }
+}
+
+var numCupones, csvLoaded = false;
+
+function loadFileCSV(t){
+  var file = t.nextElementSibling,
+      fileText = _("#msgCsvUpload"),
+      imgCheck = _("#imgCheck"),
+      close = fileText.nextElementSibling,
+      infoLoadCSV = __(".infoLoadCSV");
+  file.addEventListener("change", function(){
+    for (var i = 0; i < infoLoadCSV.length; i++) {
+      infoLoadCSV[i].style.opacity = "0";
+      infoLoadCSV[i].style.display = "none";
+    }
+    csvLoaded = true;
+    imgCheck.setAttribute("src", "ui/img/ic/check.svg");
+    fileText.innerHTML = file.files[0].name;
+    close.setAttribute("class", "displayBlock trans5");
+  });
+}
+
+function cleanCsv(t){
+  var form = _("#formCSV"),
+      input = _("#couponsUpload"),
+      text = _("#msgCsvUpload"),
+      imgCheck = _("#imgCheck"),
+      infoLoadCSV = __(".infoLoadCSV");
+      csvLoaded = false;
+      for (var i = 0; i < infoLoadCSV.length; i++) {
+        infoLoadCSV[i].style.opacity = "0";
+        infoLoadCSV[i].style.display = "none";
+      }
+      imgCheck.setAttribute("src", "ui/img/ic/upload.svg");
+      t.setAttribute("class", "displayNone trans5");
+      text.innerHTML = "Escoge un archivo .csv";
+}
+
+
+function getNumCSV(){
+    var textCSVLoaded = _(".numCSV"),
+        textCSVNoLoaded = _(".noneNumCSV"),
+        numCSVW = _("#numCSV");
+    if(csvLoaded){
+      numCSVW.innerHTML = numCupones;
+      textCSVLoaded.style.display = "block";
+      setTimeout(function(){
+        textCSVLoaded.style.opacity = "1";
+      },500);
+    } else {
+      textCSVNoLoaded.style.display = "block";
+      setTimeout(function(){
+        textCSVNoLoaded.style.opacity = "1";
+      },500);
+    }
+}
+
+
+function checkSteps(n, t){
+  switch (n) {
+    case 1:
+      checkConfig_1(n,t);
+    break;
+    case 2:
+    console.log("Selección De Funcionalidad");
+      responseStep(n , t, 1);
+    break;
+    case 3:
+      console.log("Configuración Funcionalidad");
+      responseStep(n , t, 1);
+    break;
+    case 4:
+      console.log("Selección de Plantilla");
+      responseStep(n , t, 1);
+    break;
+    case 5:
+      console.log("Edición de Plantilla");
+      responseStep(n , t, 1);
+    break;
+  }
+}
+
+function checkConfig_1(n, t){
+  /* Comprobar Configuración 1 */
+  //NUM SLIDER, THIS, nombre, descripción, marca, fecha Inicio, fecha Final
+  responseConfig_1(n, t,1,1,1,1,1,1);
+}
+
+function responseConfig_1(ns,t,n, d, m, p, fInit, fLast){
+  var g = n + d + m + p + fInit + fLast;
+  var labels = __(".labelData1"),
+      inputs = __(".inputData1");
+  if(g === 6){
+    for (var i = 0; i < labels.length; i++) {
+      labels[i].setAttribute("style", " ");
+      inputs[i].setAttribute("style", " ");
+    }
+    responseStep(ns, t, 1);
+  } else if (g < 6) {
+    responseStep(ns, t, 0);
+  }
+  if(n === 0){ redField(0); } if(d === 0){ redField(1); } if(m === 0){ redField(2); } if(p === 0){ redField(3); } if(fInit === 0){ redField(4); } if(fLast === 0){ redField(5); }
+  function redField(n){
+    labels[n].style.color = "#D8353D";
+    inputs[n].style.color = "#D8353D";
+  }
+}
+
+function responseStep(n, t, c){
+  var msgW = t.parentElement.parentElement.children[0],
+      msgTx = msgW.children[0];
+  if(c === 0){
+    msgW.style.height = "40px";
+    msgW.style.opacity = "1";
+  } else if (c === 1) {
+    msgW.setAttribute("style", " ");
+    sliderConfigFun(n);
   }
 }
