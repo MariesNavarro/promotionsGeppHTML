4c0acf65c46d1ba992e719103b89d7be4a74e2f0
Javier Corona Moreno <jackshel_10@hotmail.com>
2019-07-18 19:04:21 -0500

    imagen actualizar
    imagen

--- upload.php
+++ upload.php
@@ -10,9 +10,11 @@ if (isset($_FILES) && !empty($_FILES)) {
     else {
       $name = $id.'_'.$file['name'];
     }
-    $name = $id.'_'.$file['name'];
+    $fecha = date_create();
+
+    $name = $id.'_'.date_timestamp_get($fecha).'_'.$file['name'];
     $path = $file['tmp_name'];
-    $dir_subida = $_POST["dir"];;
+    $dir_subida = $_POST["dir"];
     $fichero_subido = $dir_subida.$name;
     if (move_uploaded_file($path, $fichero_subido)) {
       echo $name;
